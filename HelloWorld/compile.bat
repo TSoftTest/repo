32c9bd0c36592da7b3ab26d69dd1aae3d4d1828f
Diego <diegobuzzalino@yahoo.com.ar>
2016-12-23 12:17:09 -0300

    .

--- HelloWorld/compile.bat
+++ HelloWorld/compile.bat
@@ -1,2 +1,2 @@
 @echo Generating WAR file
-"C:\Program Files\WinRAR\winrar.exe" a -afzip -ep1 -r -ibck "hw.war" "WebContent\*.*"
+"C:\Program Files (x86)\WinRAR\winrar.exe" a -afzip -ep1 -r -ibck "hw.war" "WebContent\*.*"
