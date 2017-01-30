4810861d69f53a0e157beae774b9671faad0c27f
Diego <diegobuzzalino@yahoo.com.ar>
2017-01-30 12:13:46 -0300

    .

---
+++ HelloWorldBackEnd/generateJar.bat
@@ -0,0 +1,2 @@
+@echo Generating JAR file
+"C:\Program Files (x86)\WinRAR\winrar.exe" a -afzip -ep1 -r -ibck "hwbe.jar" "bin\*.*"
---
+++ HelloWorldBackEnd/src/com/hwb/HelloWorldMessage.java
@@ -0,0 +1,9 @@
+package com.hwb;
+
+public class HelloWorldMessage {
+	private String message = "Hello World!";
+
+	public String getMessage() {
+		return this.message;
+	}
+}
